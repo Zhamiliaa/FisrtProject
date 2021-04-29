cc37050e15890637f44fc548ce19fba2eec19a11
Zhamiliaa <diushalievazhamilia@gmail.com>
2021-04-29 21:07:24 +0600

    home work functions

--- dictionary.py
+++ dictionary.py
@@ -89,11 +89,92 @@
 #
 # to_file(users)
 
-dict1 = {'a':1, 'b': 3, "n": 6}
-dict2 = {'c':33, 'w':2, 'x':2}
+# dict1 = {'a':1, 'b': 3, "n": 6}
+# dict2 = {'c':33, 'w':2, 'x':2}
+#
+# dict1.update(dict2)
+# print(dict1)
 
-dict1.update(dict2)
-print(dict1)
+
+users = [{'id':1,'user': 'digital', },
+         {'id':2,'user': 'maksim', },
+         {'id':3,'user': 'vova', },
+         {'id':4,'user': 'begimai', },
+         {'id':5,'user': 'aliya', },
+         {'id':6,'user': 'bakyt',},
+         {'id':7,'user': 'chingiz', },
+         {'id':8,'user': 'jamilya',},
+         {'id':9,'user': 'cholpon', },
+         {'id':10,'user': 'erbol', }
+         ]
+
+posts = [{'id':1,'text':'hiring','status':'sponsored','keyword':'nic1'},
+         {'id':2,'text':'text1','status':'pub'},
+         {'id':3,'text':'text2','status':'pub'},
+         {'id':4,'text':'text3','status':'pub'},
+         {'id':5,'text':'text4','status':'pub'},
+         {'id':6,'text':'text5','status':'pub'},
+         {'id':7,'text':'Canada immigration','status':'sponsored','keyword':'interested!'}]
+
+
+comments = [{'id':1,'user_id':1,'post_id':1,'comment':'This is not nic1'},
+            {'id':2,'user_id':2,'post_id':1,'comment':'Hello this is nic1'},
+            {'id':3,'user_id':3,'post_id':2,'comment':'looking good!'},
+            {'id':4,'user_id':4,'post_id':2,'comment':'awesome!'},
+            {'id':5,'user_id':5,'post_id':2,'comment':'LGTM'},
+            {'id':6,'user_id':6,'post_id':3,'comment':'here we go!'},
+            {'id':7,'user_id':6,'post_id':4,'comment':'not ok'},
+            {'id':8,'user_id':6,'post_id':7,'comment':'interested!'},
+            {'id':9,'user_id':6,'post_id':7,'comment':'woohoo!'},
+            {'id':10,'user_id':6,'post_id':7,'comment':'interested!'}]
+"""
+1. Нужно найти все posts со статусом sponsored!
+2. Найти комментарии к posts sponsored
+3. Среди найденных комментариев найти те что содержат keyword из posts
+"""
+post2 = []
+def posts1 (posts):
+    """
+
+    :param posts:
+    :return:
+    """
+    for post in posts:
+        if post['status'] == 'sponsored':
+            post2.append(post)
+    return post2
+
+posts = posts1(posts)
+
+
+def comment_to_post(posts):
+    for post in posts:
+        post['comments'] = []
+        for comment in comments:
+            if post['id'] == comment['post_id']:
+                post['comments'].append(comment['comment'])
+    return posts
+
+posts = comment_to_post(posts)
+
+
+def keyword(posts):
+    for post in posts:
+        key = post['keyword']
+        for comment in post['comments']:
+            if key not in comment:
+                post['comments'].remove(comment)
+    return posts
+posts = keyword(posts)
+print(posts)
+
+
+
+
+
+
+
+
 
 
 
